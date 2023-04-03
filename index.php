ad38a7ea64f955c82caa75edf0dc3f5015b2d5c5
Shashank Verma <shashank.verma2002@gmail.com>
2023-04-03 22:24:01 +0530

    index.php: Remove dark property from cards
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- index.php
+++ index.php
@@ -78,13 +78,13 @@
     </div>
 
     <div class="flex m-8">
-        <div class="flex flex-col rounded-lg bg-white shadow-lg dark:bg-neutral-700 md:max-w md:flex-row">
+        <div class="flex flex-col rounded-lg shadow-lg bg-neutral-700 md:max-w md:flex-row">
             <img class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="static/images/aks.jpeg" alt="" />
             <div class="flex flex-col justify-start p-6">
-                <h5 class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+                <h5 class="mb-2 text-xl font-medium text-neutral-50">
                     From the President's Desk
                 </h5>
-                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                <p class="mb-4 text-base text-neutral-200">
                     Motilal Nehru National Institute of Technology Allahabad has been known for its excellence,
                     academically, and the students keep raising the bar for themselves by proving to be a step ahead
                     of the crowd, time and again. The institute, at the same time, also has records of achievements in
@@ -104,7 +104,7 @@
                     SAC.<br>
                     I wish a very bright and sporty career to all the MNNIT students.<br>
                 </p>
-                <p class="text-xs text-neutral-500 dark:text-neutral-300">
+                <p class="text-xs text-neutral-300">
                     Prof. Anil Kumar Singh <br>
                     President Student Activity Center (SAC)
                 </p>
